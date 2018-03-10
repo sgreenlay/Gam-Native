a25d5ef2e6e3073a6c87b35a9c132ad5c6096a30
Scott Greenlay <sgreenlay@gmail.com>
2018-03-10 07:05:32 -0800

    Simple rendering

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
-extern "C" void logInt(int n);
+extern "C" void log(int n);
 
-extern "C" int add(int a, int b)
+extern "C" void drawRect(int x, int y, int w, int h, int r, int g, int b, float a);
+
+typedef struct {
+    int width;
+    int height;
+} window;
+
+static window g_window;
+
+extern "C" void init(int w, int h)
 {
-    logInt(a + b);
-    return (a + b);
+    g_window.width = w;
+    g_window.height = h;
+}
+
+extern "C" void render()
+{
+    drawRect(0, 0, g_window.width, g_window.height, 255, 255, 255, 1.0f);
+    drawRect(
+        (g_window.width / 2) - 50,
+        (g_window.height / 2) - 50,
+        100,
+        100,
+        255, 0, 0, 1.0f);
 }
