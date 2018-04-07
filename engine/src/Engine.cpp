ea6587c0f526828e6c7da9f5e4c45e1b991d9758
Scott Greenlay <sgreenlay@gmail.com>
2018-04-07 14:13:07 -0700

    Move string format from Engine->Util

--- engine/src/Engine.cpp
+++ engine/src/Engine.cpp
@@ -9,27 +9,6 @@
 
 extern "C" int atexit(void (*func)()) { return 0; }
 
-char* strfmt_nz(char* out, int n)
-{
-    auto b = (char*)(&n+1)-1;
-    auto e = b - sizeof(n);
-
-    for (auto c = b; c != e; --c)
-    {
-        auto c1 = (unsigned char)(*c) % 16;
-        auto c2 = (unsigned char)(*c) >> 4;
-        *out++ = c2 < 10 ? '0' + c2 : 'a' + c2 - 10;
-        *out++ = c1 < 10 ? '0' + c1 : 'a' + c1 - 10;
-    }
-    return out;
-}
-
-char* strfmt_nz(char* out, const char* s)
-{
-    while (*s != 0) *out++ = *s++;
-    return out;
-}
-
 typedef struct {
     int width;
     int height;
